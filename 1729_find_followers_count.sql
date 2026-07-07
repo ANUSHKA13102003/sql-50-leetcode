e8fcac013201e62e9e1aaadae0b0029162645950
ANUSHKA <anushka20031013@gmail.com>
2026-07-07 20:20:00 +0530

    add solution for 1729

---
+++ 1729_find_followers_count.sql
@@ -0,0 +1,12 @@
+-- =====================================================
+-- LeetCode 1729: Find Followers Count
+-- Difficulty: Easy
+-- Language: MySQL
+-- =====================================================
+
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id;
