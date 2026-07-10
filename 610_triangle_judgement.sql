b7ebabc0e816a2e02f5710676ebcf467585457a5
ANUSHKA <anushka20031013@gmail.com>
2026-07-10 22:06:45 +0530

    add solution for 610

---
+++ 610_triangle_judgement.sql
@@ -0,0 +1,18 @@
+-- =====================================================
+-- LeetCode 610: Triangle Judgement
+-- Difficulty: Easy
+-- Language: MySQL
+-- =====================================================
+
+SELECT
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z
+         AND x + z > y
+         AND y + z > x
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
